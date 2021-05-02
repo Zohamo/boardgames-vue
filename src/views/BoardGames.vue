633b4b955fdcde56dc2722e4dc23d8787e190282
Zohamo <thiazsch@gmail.com>
2021-05-02 18:48:31 +0200

    feat(games) filters drawer

--- src/views/BoardGames.vue
+++ src/views/BoardGames.vue
@@ -9,74 +9,125 @@
       :sort-desc="sortDesc"
       hide-default-footer
     >
-      <template v-slot:header>
-        <v-toolbar class="my-2">
-          <v-row>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                v-model="search"
-                clearable
-                fixed
-                flat
-                solo
-                filled
-                hide-details
-                prepend-inner-icon="mdi-magnify"
-                label="Rechercher"
-              />
-            </v-col>
-
-            <v-col
-              v-show="$vuetify.breakpoint.mdAndUp"
-              cols="12"
-              sm="4"
-              class="d-flex align-center"
-            >
-              <v-select
-                v-model="sortBy"
-                flat
-                solo
-                filled
-                hide-details
-                :items="attributes"
-                item-text="label"
-                item-value="key"
-                prepend-inner-icon="mdi-sort"
-                label="Trier par"
-                class="mx-4"
-              ></v-select>
-              <v-btn-toggle v-model="sortDesc" mandatory>
-                <v-btn depressed :value="false">
-                  <v-icon>mdi-arrow-up</v-icon>
-                </v-btn>
-                <v-btn depressed :value="true">
-                  <v-icon>mdi-arrow-down</v-icon>
-                </v-btn>
-              </v-btn-toggle>
-            </v-col>
-
-            <v-col cols="12" sm="5" class="d-flex align-center">
-              <span class="primary--text">{{ filter.players[0] }}</span>
-              <v-range-slider
-                v-model="filter.players"
-                :max="rangePlayers.max"
-                :min="rangePlayers.min"
-                hide-details
-                class="align-center"
-              />
-              <span class="primary--text">{{ filter.players[1] }}</span>
-              <v-btn-toggle v-model="filterOn" class="ml-2">
-                <v-btn depressed :value="true">
-                  <v-icon>mdi-filter</v-icon>
-                </v-btn>
-              </v-btn-toggle>
-            </v-col>
-          </v-row>
-        </v-toolbar>
-      </template>
-
       <template v-slot:default="{ items }">
         <v-row>
+          <v-col class="flex-grow-0">
+            <v-navigation-drawer
+              v-model="drawer"
+              :mini-variant.sync="mini"
+              floating
+              permanent
+              mini-variant-width="64"
+            >
+              <v-list-item dense class="pl-1">
+                <v-list-item-content>
+                  <v-text-field
+                    v-model="search"
+                    clearable
+                    fixed
+                    flat
+                    solo
+                    filled
+                    hide-details
+                    prepend-inner-icon="mdi-magnify"
+                    label="Rechercher"
+                  />
+                </v-list-item-content>
+
+                <v-btn icon @click.stop="mini = !mini">
+                  <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+              </v-list-item>
+
+              <v-list-item dense class="pl-1">
+                <v-select
+                  v-model="sortBy"
+                  flat
+                  solo
+                  filled
+                  hide-details
+                  :items="attributes"
+                  item-text="label"
+                  item-value="key"
+                  prepend-inner-icon="mdi-sort"
+                  label="Trier par"
+                />
+                <v-btn
+                  @click="sortDesc = !sortDesc"
+                  plain
+                  small
+                  class="px-0"
+                  style="min-width: inherit"
+                >
+                  <v-icon
+                    v-text="sortDesc ? 'mdi-arrow-up' : 'mdi-arrow-down'"
+                  />
+                </v-btn>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon class="mr-1">
+                  <v-icon>mdi-filter</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-title>Filtres</v-list-item-title>
+              </v-list-item>
+
+              <v-list v-show="!mini" dense>
+                <v-list-item class="justify-space-between">
+                  <v-btn-toggle v-model="filtersActive.players">
+                    <v-btn
+                      small
+                      depressed
+                      outlined
+                      v-text="'Nombre de joueurs'"
+                      color="primary"
+                      :value="true"
+                    />
+                  </v-btn-toggle>
+                  <v-btn-toggle v-model="filtersActive.playersRange">
+                    <v-btn
+                      small
+                      depressed
+                      outlined
+                      class="px-0 ml-1"
+                      style="min-width: inherit"
+                      color="primary"
+                      :value="true"
+                    >
+                      <v-icon v-text="'mdi-arrow-left-right'" />
+                    </v-btn>
+                  </v-btn-toggle>
+                </v-list-item>
+
+                <v-list-item class="mt-2">
+                  <v-list-item-content style="overflow: visible">
+                    <v-slider
+                      v-show="!filtersActive.playersRange"
+                      v-model="filtersValue.players"
+                      :max="rangePlayers.max"
+                      :min="rangePlayers.min"
+                      hide-details
+                      dense
+                      thumb-label="always"
+                      thumb-size="16"
+                    />
+                    <v-range-slider
+                      v-show="filtersActive.playersRange"
+                      v-model="filtersValue.playersRange"
+                      :max="rangePlayers.max"
+                      :min="rangePlayers.min"
+                      hide-details
+                      dense
+                      thumb-label="always"
+                      thumb-size="16"
+                    />
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-navigation-drawer>
+          </v-col>
+
           <v-col v-for="game in items" :key="game.id">
             <GameCard :game="game" />
           </v-col>
@@ -104,13 +155,20 @@ export default Vue.extend({
     games: [] as Game[],
     filteredGames: [] as Game[],
     itemsPerPage: -1,
-    search: "" as string,
-    filterOn: false,
-    filter: {
-      players: [2, 6],
+
+    drawer: true,
+    mini: true,
+    search: "",
+    filtersActive: {
+      players: false,
+      playersRange: false,
     },
-    sortDesc: false as boolean,
-    sortBy: "bggWeight" as string,
+    filtersValue: {
+      players: 4,
+      playersRange: [2, 6],
+    },
+    sortDesc: false,
+    sortBy: "bggWeight",
     attributes: [
       {
         label: "Difficulté",
@@ -153,14 +211,21 @@ export default Vue.extend({
   },
 
   watch: {
-    filterOn: function (val) {
-      this.filteredGames = val ? this.filterPlayers() : [...this.games];
-    },
-    filter: {
+    filtersActive: {
       handler: function (val, oldVal) {
-        this.filteredGames = this.filterOn
-          ? this.filterPlayers()
-          : [...this.games];
+        this.filteredGames =
+          val.players || val.playersRange
+            ? this.filterPlayers()
+            : [...this.games];
+      },
+      deep: true,
+    },
+    filtersValue: {
+      handler: function (val, oldVal) {
+        this.filteredGames =
+          this.filtersActive.players || this.filtersActive.playersRange
+            ? this.filterPlayers()
+            : [...this.games];
       },
       deep: true,
     },
@@ -183,10 +248,10 @@ export default Vue.extend({
         if (!game.players.max) {
           game.players.max = game.players.min;
         }
-        return (
-          game.players.min <= this.filter.players[0] &&
-          game.players.max >= this.filter.players[1]
-        );
+        const [min, max] = this.filtersActive.playersRange
+          ? this.filtersValue.playersRange
+          : Array(2).fill(this.filtersValue.players);
+        return game.players.min <= min && game.players.max >= max;
       });
     },
   },
